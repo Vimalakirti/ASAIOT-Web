ec233ed1d5608d61378744843a757217feaff567
Vimalakirti <binjastudy100@gmail.com>
2021-10-13 15:58:14 +0800

    add student and academic info

--- src/Components/Academic.js
+++ src/Components/Academic.js
@@ -5,7 +5,7 @@ import {
     Container,
     Grid,
     Header,
-    Image,
+    List,
     Segment,
     Divider,
 } from "semantic-ui-react"
@@ -26,44 +26,86 @@ const Academic = () => {
             <Container>
                 <Divider horizontal>
                     <Header as='h1'>
-                    Transfer Credits
-                    </Header>
-                </Divider>
-                <Divider horizontal>
-                    <Header as='h1'>
                     Prerequisite Requirement
                     </Header>
                 </Divider>
+                <List bulleted size='huge'>
+                    <List.Item>
+                        <List.Content>
+                        Students will take two exams, Programming and Probabilities & Statistics at the beginning of fall semester.
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        Passing the similar course (at the schools or online) is not allowed in lieu of this requirement.
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        The prerequisite exam is offered at the beginning of every fall semester. Each student can take it twice at most. The student who is not able to fulfill this requirement at the beginning of the third semester will be dismissed from the program.
+                        </List.Content>
+                    </List.Item>
+                </List>
                 <Divider horizontal>
                     <Header as='h1'>
                     Outstanding Academic Performance
                     </Header>
                 </Divider>
+                <List bulleted size='huge'>
+                    <List.Item>
+                        <List.Content>
+                        Students are required to high pass 2 AIoT core courses. Superior graduate level performance. 
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        The students are allowed to re-take the course and obtain "high pass." In order to meet qualifying exam grade requirement, the students can repeat this course but credit hours shall not be counted as graduation requirements.
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        Publication or paper submission is not allowed in lieu of this requirement.
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        Students who are not able to pass 2 AIoT core courses WITH superior graduate level performance by the end of the fifth semester will be dismissed from the program.
+                        </List.Content>
+                    </List.Item>
+                </List>
                 <Divider horizontal>
                     <Header as='h1'>
-                    Degree Requirement
+                    Professional Subjects Assessment
                     </Header>
                 </Divider>
+                <p>Students who enroll at NTU will need to accomplish 18(???) credits of Ph.D. level course and the grades must be at least B+ within 5 semesters after enrollment.</p>
                 <Divider horizontal>
                     <Header as='h1'>
-                    Qualifying Examination
+                    Research Ability Assessment
                     </Header>
                 </Divider>
-                <Divider horizontal>
-                    <Header as='h1'>
-                    Degree Examination
-                    </Header>
-                </Divider>
-                <Divider horizontal>
-                    <Header as='h1'>
-                    Progress Report
-                    </Header>
-                </Divider>
-                <Divider horizontal>
-                    <Header as='h1'>
-                    Degree Conferral
-                    </Header>
-                </Divider>
+                <List bulleted size='huge'>
+                    <List.Item>
+                        <List.Content>
+                        The application for Ph.D. Thesis proposal is only accepted after the student have accomplished "Outstanding Academic Performance" requirement (and Qualifying Exam of the partner university.)
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        The deadline for Thesis Proposal Approval is by end of the third year.
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        The assessment committee will be composed of three faculty members: advisor, coadvisor and one AIoT Academic Committee member.
+                        </List.Content>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        The research ability assessment may be held twice. Student who do not pass the second assessment must withdraw from the PhD program in the following semester.
+                        </List.Content>
+                    </List.Item>
+                </List>
             </Container>
         </div>
     )
--- src/Components/Student.js
+++ src/Components/Student.js
@@ -5,7 +5,7 @@ import {
     Container,
     Grid,
     Header,
-    Image,
+    List,
     Segment,
     Divider,
 } from "semantic-ui-react"
@@ -29,11 +29,74 @@ const Student = () => {
                     Thesis Advisor
                     </Header>
                 </Divider>
+                <p>TODO</p>
+                <p>The TIGP on Social Networks and Human-Centered Computing cuts into social networks and human-centered computing mainly from the viewpoint of computer science and engineering, and is supplemented by social and behavioral science, to provide students in the program with interdisciplinary training.</p>
+                <p>This program is completely taught in English. The courses include core courses, elective courses, seminar and project research. Program professors guide students to the newest research topics in the field of social network and human-centered computing to conduct high-quality, cutting-edge research.</p>
+                <p>This program adopts a team-teaching system, and students should decide their advisor and co-advisor within the first academic year after enrollment. The student should complete Advisor/Co-advisor Consent Form and return it to the SNHCC office. It shall be approved by the academic committee members.</p>
+                <p>The advisor/co-advisor for the thesis dissertation must be SNHCC faculty. A student must choose an advisor from Academic Sinica or the university he/she enrolls.</p>
+                <p>For example, if you enroll in NCCU, you may choose advisor from AS and a co-advisor from NCCU (or you may choose an advisor from NCCU and a co-advisor from AS). But a NCCU student can't choose NTHU teacher to be your advisor, but this NTHU teacher can be your one of your co-advisors.</p>
+                <p>If you want to change your advisor or co-advisor, you need to complete Petition for Change of Advisor. It shall be approved by the academic committee members.</p>
+                <p>he forms are available in the TIGP SNHCC office. The teachers who are eligible to take students will be announced at the end of each semester.</p>
                 <Divider horizontal>
                     <Header as='h1'>
                     Stipend Guideline
                     </Header>
                 </Divider>
+                <p>Stipend for the first year is guaranteed; the amount of the stipend for the second and third years will depend on student's performance. In the subsequent year, the financial support will be provided by the student's thesis advisor. The amount of the support will be at the discretion of the advisor. Violations of ethics, e.g. plagiarism, unauthorized absence from class, distasteful conduct are considered as "bad" performance and may result in reduced allowance.</p>
+                <List ordered size='huge'>
+                    <List.Item>
+                        <List.Content>
+                        Semester Evaluation by Mentor
+                        </List.Content>
+                        <List>
+                            <List.Item>
+                                Mentor
+                                <List>
+                                    <List.Item>&nbsp;&nbsp;Each student is required to find one mentor within a month after the semester starts.The mentor will give the student guidance or advises as needed. The mentor may offer suggestions on how to find advisor, participate in a lab activity or project, access to a professional community, recommend resources, lectures or conferences, and others. The mentorship will last until the student has officially found the thesis advisor. (The mentor will not necessary be your advisor in the future.)</List.Item>
+                                    <List.Item>&nbsp;&nbsp;The mentor can be from Academia Sinica or the partner university one is enrolled. Each mentor can take two students at most.</List.Item>
+                                    <List.Item>&nbsp;&nbsp;A mentee will need to take initiative to arrange meetings with the mentor in a regular basis.</List.Item>
+                                    <List.Item>&nbsp;&nbsp;Complete Mentor Consent Form a month after the semester starts.</List.Item>
+                                </List>
+                            </List.Item>
+                            <List.Item>
+                                Evaluation by Mentor
+                                <List>
+                                    <List.Item>&nbsp;&nbsp;In the end of every semester, the mentor will evaluate the student's academic performance based on attendance, grades, attitude, participation and enthusiasm upon the PhD studies. The evaluation form needs to be filled and signed by the mentor.</List.Item>
+                                    <List.Item>&nbsp;&nbsp;In addition, students should submit the transcript and supporting materials, such as lab report, current study plan to AIoT office before the semester ends. If the mentee remains good standing, he/she will receive full amount of stipend for the following semester.</List.Item>
+                                </List>
+                            </List.Item>
+                            <List.Item>
+                                Evaluation by Student Affairs Committee
+                                <div>The purpose of stipend is to enhance and support the overall educational experiences. If the mentee's performance is not satisfactory, the Student Affairs Committee reserves the right to deduct the amount of stipend.</div>
+                            </List.Item>
+                            <List.Item>
+                                Review and Notification
+                                <div>The review will be held after the grade for each course is completed every semester. The result will be emailed to each student individually at the beginning of the following semester (for fall semester result), or by the end of July (for spring semester). If there is stipend deduction, it will start from March or September.</div>
+                            </List.Item>
+                        </List>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        Advisor
+                        </List.Content>
+                        <List>
+                            <List.Item>If a student cannot find thesis advisor by the end of third semester, the stipend will be terminated.</List.Item>
+                            <List.Item>For the fourth year students, the financial support will be provided by the thesis advisor and the amount will not be guaranteed to be the same as TIGP stipend.</List.Item>
+                        </List>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        Misconduct
+                        </List.Content>
+                        <div>Any serious on-campus, off-campus, scholarly misconduct will be evaluated by the Student Affairs Committee for reduced allowance. For serious violation or misconduct, e.g. plagiarism, stealing, etc, the procedure shall be done in accordance with the Student Award and Penalty Regulations of NTU.</div>
+                    </List.Item>
+                    <List.Item>
+                        <List.Content>
+                        Amendment
+                        </List.Content>
+                        <div>The rules and regulations stated in this document shall be administered after they are approved by Student Affairs Committee. Any revision to this guideline must follow the same procedures.</div>
+                    </List.Item>
+                </List>
             </Container>
         </div>
     )
